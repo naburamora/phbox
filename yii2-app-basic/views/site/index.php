511b1f21ab95c3e7adcfb0f86efff02a2e4427e0
naburamora <naburamor@gmail.com>
2017-03-04 21:39:57 +0000

    yii2 app

---
+++ yii2-app-basic/views/site/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Добро пожаловать!</h1>
+
+        <p class="lead">Вы успешно создали веб-приложение, построенное на Yii Framework 2.0!</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Открыть документацию по разработке на Yii2</a></p>
+    </div>
+
+    <div class="body-content">
+    <div id="row">
+        <article>123</article>
+    </div>
+
+    </div>
+</div>
